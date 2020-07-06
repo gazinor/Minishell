dec5c7e719622cf361eeaf358946258a049aaa40
Gaylor LAURENT <glaurent@e1r11p4.42.fr>
2020-07-06 22:17:46 +0200

    env fonctionne pour les retours et les erreurs

--- srcs/main_loop.c
+++ srcs/main_loop.c
@@ -6,7 +6,7 @@
 /*   By: gaefourn <gaefourn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/10 02:16:51 by gaefourn          #+#    #+#             */
-/*   Updated: 2020/07/06 20:29:05 by gaefourn         ###   ########.fr       */
+/*   Updated: 2020/07/06 22:04:49 by glaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ int		display_output(t_data *data, char *tmp)
 	if (ft_redir(data, data->cmd_lst->pipe->cmd) == -1)
 		return (redir_error(data));
 	if (is_builtin(data->cmd_lst->pipe->cmd, data) == 1)
-		data->check_ret == 1 ? data->ret = 1 : 0;
+		data->check_ret > 0 ? data->ret = data->check_ret : 0;
 	else if ((data->binary = is_exec(data->cmd_lst->pipe->cmd, data)) != NULL)
 	{
 		check_ls(data->cmd_lst->pipe->cmd, data);
